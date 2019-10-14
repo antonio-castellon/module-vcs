fbf02d954d2df84038e18de42656cede5d10d58e
Castellon, Antonio (contracted) <Antonio.Castellon@contracted.pmi.com>
2019-10-14 13:33:15 +0200

    minor refactorization

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 //
 // test module
 //
-const vcs = require('@acastellon/vcs.js');
+const vcs = require('@acastellon/vcs');
 
 vcs.getHash('./test.js').then(function(value) { console.log(value); });
